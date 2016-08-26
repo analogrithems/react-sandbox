8445c69dc9bce0475a0e2ef8ea72dcd45bb36a24
Aaron C <aaron@digitalmediums.com>
2016-08-25 21:53:36 -1000

    continue to use hjs, override the babel-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const path = require('path');
 const join = path.join;
 const resolve = path.resolve;
 
+const getConfig = require('hjs-webpack');
 
 const isDev = NODE_ENV === 'development';
 const isTest = NODE_ENV === 'test';
@@ -23,171 +24,6 @@ const src = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest = join(root, 'dist');
 const css = join(src, 'styles');
-
-var HtmlPlugin = require('./html-plugin')
-var pick = require('lodash.pick')
-var isInstalled = require('./is-installed')
-
-const getConfig = function(spec) {
-  var baseConfig = {
-    entry: [
-      spec.entry
-    ],
-    output: spec.output,
-    resolve: {
-      extensions: [
-        '',
-        '.js',
-        '.jsx',
-        '.json'
-      ]
-    },
-    plugins: [
-      new HtmlPlugin(pick(spec, [
-        'html',
-        'isDev',
-        'serveCustomHtmlInDev',
-        'package'
-      ]))
-    ],
-    module: {
-      loaders: []
-    }
-  }
-
-  var optionalBaseLoaders = [
-    {
-      pkg: 'worker-loader',
-      config: {
-        test: /(^|\.)worker\.js$/,
-        exclude: /node_modules/,
-        loaders: ['worker-loader']
-      }
-    },
-    {
-      pkg: 'worker-loader',
-      config: {
-        test: /(^|\.)thread\.js$/,
-        exclude: /node_modules/,
-        loaders: ['worker-loader?inline']
-      }
-    },
-    {
-      pkg: 'babel-loader',
-      config: {
-        test: /\.(js|jsx|babel)$/,
-        include: [__dirname+"/node_modules/dd", __dirname+"/src"],
-        loader: 'babel-loader'
-      }
-    },
-    {
-      pkg: 'coffee-loader',
-      config: {
-        test: /\.coffee$/,
-        loaders: ['coffee-loader']
-      }
-    },
-    {
-      pkg: 'cjsx-loader',
-      config: {
-        test: /\.cjsx$/,
-        loaders: ['coffee-loader', 'cjsx-loader']
-      }
-    },
-    {
-      pkg: 'awesome-typescript-loader',
-      config: {
-        test: /\.(ts|tsx)$/,
-        loaders: ['awesome-typescript-loader']
-      }
-    },
-    {
-      pkg: 'livescript-loader',
-      config: {
-        test: /\.ls$/,
-        loaders: ['livescript-loader']
-      }
-    },
-    {
-      pkg: 'json-loader',
-      config: {
-        test: /\.json$/,
-        loaders: ['json']
-      }
-    },
-    {
-      pkg: 'url-loader',
-      config: {
-        test: /\.otf(\?\S*)?$/,
-        loader: 'url-loader?limit=' + spec.urlLoaderLimit
-      }
-    },
-    {
-      pkg: 'url-loader',
-      config: {
-        test: /\.eot(\?\S*)?$/,
-        loader: 'url-loader?limit=' + spec.urlLoaderLimit
-      }
-    },
-    {
-      pkg: 'url-loader',
-      config: {
-        test: /\.svg(\?\S*)?$/,
-        loader: 'url-loader?mimetype=image/svg+xml&limit=' + spec.urlLoaderLimit
-      }
-    },
-    {
-      pkg: 'url-loader',
-      config: {
-        test: /\.ttf(\?\S*)?$/,
-        loader: 'url-loader?mimetype=application/octet-stream&limit=' + spec.urlLoaderLimit
-      }
-    },
-    {
-      pkg: 'url-loader',
-      config: {
-        test: /\.woff2?(\?\S*)?$/,
-        loader: 'url-loader?mimetype=application/font-woff&limit=' + spec.urlLoaderLimit
-      }
-    },
-    {
-      pkg: 'url-loader',
-      config: {
-        test: /\.(jpe?g|png|gif)$/,
-        loader: 'url-loader?limit=' + spec.urlLoaderLimit
-      }
-    },
-    {
-      pkg: 'jade-loader',
-      config: {
-        test: /\.jade$/,
-        loaders: ['jade']
-      }
-    }
-  ]
-
-  // Add optional loaders
-  optionalBaseLoaders.forEach(function (item) {
-    if (isInstalled(item.pkg)) {
-      baseConfig.module.loaders.push(item.config)
-    }
-  })
-
-  // Add optional plugins
-  if (isInstalled('yeticss')) {
-    baseConfig.stylus = {
-      use: [require('yeticss')()]
-    }
-  }
-
-  if (isInstalled('autoprefixer')) {
-    baseConfig.postcss = [require('autoprefixer')()]
-  }
-
-  return baseConfig
-}
-
-
 
 var config = getConfig({
   isDev: isDev || isTest,
@@ -257,6 +93,15 @@ const newloader = Object.assign({}, cssloader, {
 config.module.loaders.push(newloader);
 cssloader.test = new RegExp(`[^module]${cssloader.test.source}`);
 cssloader.loader = newloader.loader;
+
+//existing babel-loader
+const babeloader = findLoader(config.module.loaders, /(^|!)(babel-loader)($|!)/);
+const newBabelLoader = Object.assign({}, babeloader, {
+  include: [__dirname+"/node_modules/dd", __dirname+"/src"],
+  exclude: ''
+});
+config.module.loaders.push(newBabelLoader);
+
 
 
 config.module.loaders.push({
